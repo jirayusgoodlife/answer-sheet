e2d9c2a7bb4257bda811f9183b5ecc914fafac73
Jirayus Arbking <dev.jirayus@gmail.com>
2021-02-03 09:00:28 +0700

    update firebase conf

--- answer_sheet_vuejs/src/main.js
+++ answer_sheet_vuejs/src/main.js
@@ -3,8 +3,22 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANrX-5CgZDZmoa2xS7JJDOI6BUwAP2oAY",
+  authDomain: "simple-answer-sheet.firebaseapp.com",
+  databaseURL: "https://simple-answer-sheet.firebaseio.com",
+  projectId: "simple-answer-sheet",
+  storageBucket: "simple-answer-sheet.appspot.com",
+  messagingSenderId: "277125676234",
+  appId: "1:277125676234:web:d22cebdff3f9f8a7c78833",
+  measurementId: "G-V8TFXSL7GT"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
--- answer_sheet_vuejs/src/views/Home.vue
+++ answer_sheet_vuejs/src/views/Home.vue
@@ -3,10 +3,32 @@
     <div class="hero-body">
       <div class="container has-text-centered">
         <div class="column is-4 is-offset-4">
-          <div class="buttons are-medium is-justify-content-space-between">
+          <div class="buttons are-medium is-justify-content-space-between is-hidden">
             <a class="button is-primary">Create Room</a>
             <a class="button is-info">Join Room</a>
           </div>
+        </div>
+        <div class="column is-4 is-offset-4">
+          <table class="table is-bordered is-fullwidth is-hoverable">
+              <thead>
+                <td>#</td>
+                <td>Name</td>
+                <td></td>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>TOEIC no</td>
+                  <td>
+                    <a class="button is-small">
+                      <span class="icon is-small">
+                        <i class="fas fa-chevron-right"></i>
+                      </span>
+                    </a>
+                    </td>
+                </tr>
+              </tbody>
+          </table>
         </div>
       </div>
     </div>
